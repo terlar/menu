44cf7a760c7b6a93175f1116132fd77101c5ccc2
Terje Larsen <terlar@gmail.com>
2019-12-15 18:44:52 +0100

    shell.nix: remove missplaced comment

--- shell.nix
+++ shell.nix
@@ -4,6 +4,5 @@ let
   pkgs = import pkgsPath {};
   drv = import ./. { inherit pkgsPath; };
 in pkgs.mkShell {
-  # Create a shell with all Klarna Packages and some development tools
   buildInputs = [pkgs.man pkgs.less pkgs.fish drv];
 }
